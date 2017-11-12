dab637d33241339ac177a9d2acf855debb5d5378
“Tea <teateearu@gmail.com>
2017-11-12 11:25:05 +0100

    Added seeds: artists and songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+artist1 = Artist.create( name: "Mariah Carey", alive: true )
+artist2 = Artist.create( name: "Frank Sinatra", alive: false )
+artist3 = Artist.create( name: "Nat King Cole", alive: false )
+artist4 = Artist.create( name: "Celine Dion", alive: true )
+
+song1 = Song.create( title: "All I Want For Christmas Is You", year: 1994, artist: artist1 )
+song2 = Song.create( title: "Hark! The Herald Angels Sing / Gloria (In Excelsis Deo)", year: 1994, artist: artist1 )
+song3 = Song.create( title: "Have Yourself A Merry Little Christmas", year: 1947, artist: artist2 )
+song4 = Song.create( title: "White Christmas", year: 1944, artist: artist2 )
+song5 = Song.create( title: "The Christmas Song (Chestnuts Roasting On An Open Fire)", year: 1961, artist: artist3 )
+song6 = Song.create( title: "Deck The Halls", year: 1960, artist: artist3 )
+song7 = Song.create( title: "I'm Your Angel", year: 1998, artist: artist4 )
+song8 = Song.create( title: "O Holy Night", year: 1998, artist: artist4 )

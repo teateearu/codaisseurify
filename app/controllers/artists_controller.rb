3aee41dc10db3d15209a20da898778e0288e9423
“Tea <teateearu@gmail.com>
2017-11-12 18:10:24 +0100

    Fix text color inside create song form

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,19 @@
 class ArtistsController < ApplicationController
+
   def index
     @artists = Artist.all
+
+    if params[:order] == 'artists_asc'
+      @artists = Artist.all.order(:name)
+    end
+    
   end
+
   def show
     @artist = Artist.find(params[:id])
     @songs = @artist.songs
   end
+
   def destroy
     @artist = Artist.find(params[:id])
 
@@ -13,4 +21,5 @@ class ArtistsController < ApplicationController
 
     redirect_to artists_path
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Artist.destroy_all
 Song.destroy_all
 
-artist1 = Artist.create( name: "Mariah Carey", alive: true, remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412105/1400-Mariah-Carey_nc9rjy.jpg" )
-artist2 = Artist.create( name: "Frank Sinatra", alive: false, remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412251/6ac543c9bd357841e7c5eb1fcac6cc39--frank-sinatra-christmas-songs-christmas-music_wmmscx.jpg" )
-artist3 = Artist.create( name: "Nat King Cole", alive: false, remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412122/maxresdefault_vbwyoz.jpg" )
-artist4 = Artist.create( name: "Celine Dion", alive: true, remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412265/cdn162009LARGE_vi6ll7.jpg" )
-artist5 = Artist.create( name: "Michael Buble", alive: true, remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510492863/michaelbuble_vjx15y.jpg" )
-artist6 = Artist.create( name: "Kenny G", alive: true, remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_west,h_200,w_378/v1510493358/maxresdefault_gocceg.jpg" )
+artist1 = Artist.create( name: "Mariah Carey", bio: "Born March 27, 1969 or 1970. She is an American singer, songwriter, record producer, and actress." , remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412105/1400-Mariah-Carey_nc9rjy.jpg" )
+artist2 = Artist.create( name: "Frank Sinatra", bio: "Lived December 12, 1915 – May 14, 1998. He was an American singer, actor, and producer." , remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412251/6ac543c9bd357841e7c5eb1fcac6cc39--frank-sinatra-christmas-songs-christmas-music_wmmscx.jpg" )
+artist3 = Artist.create( name: "Nat King Cole", bio: "Lived March 17, 1919 – February 15, 1965. He was an American jazz pianist and vocalist.", remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412122/maxresdefault_vbwyoz.jpg" )
+artist4 = Artist.create( name: "Celine Dion", bio: "Born 30 March 1968. She is a Canadian singer and businesswoman." , remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510412265/cdn162009LARGE_vi6ll7.jpg" )
+artist5 = Artist.create( name: "Michael Buble", bio: "Born 9 September 1975. He is a Canadian singer, songwriter, actor and record producer.", remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_face,h_200,w_378/v1510492863/michaelbuble_vjx15y.jpg" )
+artist6 = Artist.create( name: "Kenny G", bio: "Born June 5, 1956. He is an American saxophonist." , remote_image_url: "http://res.cloudinary.com/teateearu/image/upload/c_fill,g_west,h_200,w_378/v1510493358/maxresdefault_gocceg.jpg" )
 
 song1 = Song.create( title: "All I Want For Christmas Is You", year: 1994, artist: artist1 )
 song2 = Song.create( title: "Hark! The Herald Angels Sing / Gloria (In Excelsis Deo)", year: 1994, artist: artist1 )

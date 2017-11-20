9ff33c0e1f55c5e8b84523f95f332b658bf04ace
“Tea <teateearu@gmail.com>
2017-11-20 23:44:02 +0100

    Song form add id's

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,13 +1,13 @@
 function createSong(artist_id) {
-  var title = $("#addTitle").val();
-  var year = $("#addYear").val();
+  var titleIs = $("#addTitle").val();
+  var yearIs = $("#addYear").val();
 
   $.ajax({
     type: "POST",
     url: "/api/artists/" + artist_id + "/songs",
     data: JSON.stringify({
-      song_title: title,
-      song_year: year
+      song_title: titleIs,
+      song_year: yearIs
     }),
     contentType: "application/json",
     dataType: "json"
@@ -33,5 +33,10 @@ function createSong(artist_id) {
 
  $(document).ready(function() {
    $("#songForm").hide();
+   $("#listSongs").show();
+   $("#addButton").click(function(){
+     $("#listSongs").hide();
+     $("#songForm").show();
+   });
    $(".delete-song").bind('click', deleteSong);
  });
